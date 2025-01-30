b8263d2cd0834903aeb975ee0cfdd12b4a1eed8e
harimau97 <harimauho@gmail.com>
2025-01-30 22:08:08 +0900

    Design: 번역문서 커버 스타일 변경

--- front/src/pages/translate/translate.jsx
+++ front/src/pages/translate/translate.jsx
@@ -41,19 +41,23 @@ const TransLatePage = () => {
           return (
             <div
               key={index}
-              className="border-black border-2 w-[300px] h-[300px] mr-[2.55%] ml-[2.5%] rounded-xl p-2"
+              className="box-border srhink-0 w-[300px] h-[300px] relative mr-2 ml-2 "
             >
-              <img
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAzFBMVEVHcExwLIZpK4PIIDjJIDfXQS85KGyRHVP1lSKPL27odCbJIjflbSiZI2S5IEeGJnWpIFewIE6rIFWrIFMqJmPQNDPEIDvKJDfLIDb0kSPFIDvxjCPeWSvFIDvJIDe/IEDfWSvfWyvXQC+0IEx+KHyFJnaDJneBJ3m8IETOKTTjainoeSf0lSPndCfLIDbeWCzjaSl+KHuKJXN6KX27IER/KHu2IEmbImWGJnV/KHvKIDazIEy/IEDOJjTaSy7hYCrqeyaJJXPpeCbdVSxkhj7EAAAAOnRSTlMANkom8usWBP0MG8P5Ht7M96m24UdcpDeviP5xPpjl+NzOwUgkqbfgk/Jx5ufdiKnelGOdyo72UWrsTLVmUAAAANZJREFUKJF90VeTgjAUhuGAwQRFql13FcGuW3TXkgSw/P//5Dhe+oVzmWfeM5mEkLfpcV57P33NgHPe09jwsuF8gK3246wvN83a7yJ2nC22Vh6Pi2IFjVnWJL/HNsSvbGLl1hBahdJ6li07ED9E/VOILkPWobQrBK3gUD3DNgxtd9RW/X4Lhol0/5Qa4a2BDA5SLaCxKAqldBsQG34YST+Blp6uoR8c8cux2dzzpjtoxP6fna9TbMTw9vNffBvCbDPVZM+1pt6IYZSgWdUba5aE1VJEv/gAhbgShVrVkEQAAAAASUVORK5CYII="
-                alt="문서 아이콘"
-              />
-              <div>{docs}</div>
-              <button
-                className="cursor-pointer border"
-                onClick={() => navigate(`/translate/viewer/${docs}`)}
-              >
-                번역하기
-              </button>
+              <div className="flex items-center justify-center flex-col rounded-[4px] border-solid border-[#787f8f] border-[0.5px] w-full h-full absolute shadow-[inset_0px_0px_10px_0px_rgba(42,42,48,0.25),_0px_7px_35px_4px_rgba(32,31,49,0.20)]">
+                <img
+                  className="w-[50%] h-[50%] relative"
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAzFBMVEVHcExwLIZpK4PIIDjJIDfXQS85KGyRHVP1lSKPL27odCbJIjflbSiZI2S5IEeGJnWpIFewIE6rIFWrIFMqJmPQNDPEIDvKJDfLIDb0kSPFIDvxjCPeWSvFIDvJIDe/IEDfWSvfWyvXQC+0IEx+KHyFJnaDJneBJ3m8IETOKTTjainoeSf0lSPndCfLIDbeWCzjaSl+KHuKJXN6KX27IER/KHu2IEmbImWGJnV/KHvKIDazIEy/IEDOJjTaSy7hYCrqeyaJJXPpeCbdVSxkhj7EAAAAOnRSTlMANkom8usWBP0MG8P5Ht7M96m24UdcpDeviP5xPpjl+NzOwUgkqbfgk/Jx5ufdiKnelGOdyo72UWrsTLVmUAAAANZJREFUKJF90VeTgjAUhuGAwQRFql13FcGuW3TXkgSw/P//5Dhe+oVzmWfeM5mEkLfpcV57P33NgHPe09jwsuF8gK3246wvN83a7yJ2nC22Vh6Pi2IFjVnWJL/HNsSvbGLl1hBahdJ6li07ED9E/VOILkPWobQrBK3gUD3DNgxtd9RW/X4Lhol0/5Qa4a2BDA5SLaCxKAqldBsQG34YST+Blp6uoR8c8cux2dzzpjtoxP6fna9TbMTw9vNffBvCbDPVZM+1pt6IYZSgWdUba5aE1VJEv/gAhbgShVrVkEQAAAAASUVORK5CYII="
+                  alt="문서 아이콘"
+                />
+                <div>{docs}</div>
+                <button
+                  className="cursor-pointer border"
+                  onClick={() => navigate(`/translate/viewer/${docs}`)}
+                >
+                  번역하기
+                </button>
+              </div>
+              <div className="bg-[linear-gradient(90deg,_rgba(0,0,0,0.00)_14.58333283662796%,_rgba(26,29,55,0.10)_48.40801954269409%,_rgba(0,0,0,0.00)_85.41666865348816%)] w-[13px] absolute left-[12px] bottom-[0px] top-[0px]"></div>
             </div>
           );
         })}
