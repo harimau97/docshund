e82582987b715f4cf9b93cf8ddeed57f5cda9a14
TinyFrogs <qns0147@gmail.com>
2025-01-31 16:36:10 +0900

    Feat: 유저 조회 기능에 대한 QueryDSL 레포지토리 구현

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/users/repository/UserRepository.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/users/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.docshund.domain.users.entity.Provider;
 import com.ssafy.docshund.domain.users.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
 	Optional<User> findByProviderAndPersonalId(Provider provider, String personalId);
 
