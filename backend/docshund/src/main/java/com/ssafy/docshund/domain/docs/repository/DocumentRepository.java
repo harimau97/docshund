55bd1658b4f69403e15cad61838a0ea3920c6c33
이승연 <snowball@sookmyung.ac.kr>
2025-02-03 12:52:02 +0900

    Feat: DocumentName으로 조회해오는 메서드 구현

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/docs/repository/DocumentRepository.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/docs/repository/DocumentRepository.java
@@ -1,5 +1,7 @@
 package com.ssafy.docshund.domain.docs.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ public interface DocumentRepository extends JpaRepository<Document, Integer> {
 
 	Document findByDocsId(Integer docsId);
 
+	Optional<Document> findByDocumentName(String documentName);
 }
