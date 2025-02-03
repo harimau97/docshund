99ab57aaac5ce24d7bb7d2277caac5a0830d1cb8
이승연 <snowball@sookmyung.ac.kr>
2025-02-03 12:55:31 +0900

    Refactor: AllArgsConstructor 추가

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/forums/dto/CommentDto.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/forums/dto/CommentDto.java
@@ -1,10 +1,12 @@
 package com.ssafy.docshund.domain.forums.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CommentDto {
     private String content;
 }
