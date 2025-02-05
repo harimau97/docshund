832f6f95741a67bcef6e10523a7c1de1881dc4d9
Jung Jieun <wldms3333@gmail.com>
2025-02-05 14:07:27 +0900

    Test: Docs Service 테스트 수정
    docker 환경에 맞춰 수정

--- backend/docshund/src/test/java/com/ssafy/docshund/domain/docs/service/DocsServiceImplTest.java
+++ backend/docshund/src/test/java/com/ssafy/docshund/domain/docs/service/DocsServiceImplTest.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
+import com.ssafy.docshund.domain.docs.entity.*;
 import com.ssafy.docshund.domain.docs.repository.*;
 import com.ssafy.docshund.domain.users.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,8 +23,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ssafy.docshund.domain.docs.dto.DocumentDto;
 import com.ssafy.docshund.domain.docs.dto.OriginDocumentDto;
 import com.ssafy.docshund.domain.docs.dto.TranslatedDocumentDto;
-import com.ssafy.docshund.domain.docs.entity.Document;
-import com.ssafy.docshund.domain.docs.entity.Position;
 import com.ssafy.docshund.domain.users.entity.Provider;
 import com.ssafy.docshund.domain.users.entity.User;
 import com.ssafy.docshund.fixture.UserTestHelper;
@@ -36,10 +35,14 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
+@DisplayName("[ Document Service Test ]")
 class DocsServiceImplTest {
 
 	@Autowired
 	private DocsService docsService;
+
+	@Autowired
+	private UserTestHelper userTestHelper;
 
 	@Autowired
 	private UserRepository userRepository;
@@ -57,24 +60,51 @@ class DocsServiceImplTest {
 	private CustomDocumentRepository customDocumentRepository;
 
 	private User user1;	// 어드민 유저
-	private User user2; // 번역 UD 테스트용 유저
+	private User user2; // 좋아요 및 번역 CUD 테스트용 유저
+	private User user3; // 좋아요 및 번역 CUD 테스트용 유저
+
+	private Document doc1;
+	private Document doc2;
+	private OriginDocument originDoc1;
+	private OriginDocument originDoc2;
+	private TranslatedDocument transDoc1;
+	private TranslatedDocument transDoc2;
 
 	@MockitoBean
 	private UserUtil userUtil;
 
-	@Autowired
-	private UserTestHelper userTestHelper;
-
 	@BeforeEach
 	public void setUp() {
 		// user repository setup
-		user1 = userRepository.findById(7L).orElse(null);
-		user2 = userRepository.findById(6L).orElse(null);
+		user1 = userTestHelper.saveUser("admin@gmail.com", "100000", "adminUser", Provider.GOOGLE, true, "안녕하세요", "Backend",
+				true);
+		user2 = userTestHelper.saveUser("test1@gmail.com", "10001", "testUser1", Provider.GOOGLE, false, "안녕하세요", "Frontend",
+				true);
+		user3 = userTestHelper.saveUser("test2@github.com", "10002", "testUser2", Provider.GITHUB, false, "안녕하세요", "Backend",
+				true);
 		// document repository setup
-		Document doc1 = documentRepository.save(new Document("Spring", "Spring Boot", "logoImage", "v1",
+		doc1 = documentRepository.save(new Document("Spring", "Spring Boot", "logoImage", "v1",
 			0, Position.BACKEND, "apache", "docLink"));
-		Document doc2 = documentRepository.save(new Document("MySQL", "MySQL", "logoImage", "v1",
+		doc2 = documentRepository.save(new Document("MySQL", "MySQL", "logoImage", "v1",
 			0, Position.DBSQL, "apache", "docLink"));
+
+		// document like repository setup
+		documentLikeRepository.save(new DocumentLike(doc1, user2));
+		documentLikeRepository.save(new DocumentLike(doc1, user3));
+		documentLikeRepository.save(new DocumentLike(doc2, user2));
+
+		// origin document repository setup
+		originDoc1 = originDocumentRepository.save(new OriginDocument(doc1, 1, "<p>", "<p>Spring Framework is the best Framework for Java.</p>"));
+		originDoc2 = originDocumentRepository.save(new OriginDocument(doc1, 2, "<p>", "<p>And Spring boot is very easy and simple.</p>"));
+
+		// translated document repository setup
+		transDoc1 = translatedDocumentRepository.save(new TranslatedDocument(originDoc1, user2, "스프링 프레임워크는 자바를 위한 최고의 프레임워크입니다.", 0, Status.VISIBLE));
+		transDoc2 = translatedDocumentRepository.save(new TranslatedDocument(originDoc2, user3, "그리고 스프링 부트는 아주 쉽고 간단합니다.", 0, Status.VISIBLE));
+
+		// translated document like repository setup
+		translatedDocumentLikeRepository.save(new TranslatedDocumentLike(transDoc1, user2));
+		translatedDocumentLikeRepository.save(new TranslatedDocumentLike(transDoc2, user3));
+
 	}
 
 	@Test
@@ -96,11 +126,9 @@ class DocsServiceImplTest {
 	@Test
 	@DisplayName("특정 문서 상세 조회 테스트")
 	void getDocumentDetail() {
-		// given
-		Integer docsId = 1;
 
 		// when
-		DocumentDto result = docsService.getDocumentDetail(docsId);
+		DocumentDto result = docsService.getDocumentDetail(doc1.getDocsId());
 
 		// then
 		assertThat(result).isNotNull();
@@ -141,11 +169,9 @@ class DocsServiceImplTest {
 	@WithMockCustomOAuth2User
 	@DisplayName("문서 좋아요 등록/취소 테스트")
 	void toggleLikes() {
-		// given
-		Integer docsId = 1;
 
 		// when
-		DocumentDto result = docsService.toggleLikes(docsId, user1);
+		DocumentDto result = docsService.toggleLikes(doc2.getDocsId(), user3);
 
 		// then
 		assertThat(result).isNotNull();
@@ -183,7 +209,6 @@ class DocsServiceImplTest {
 	@DisplayName("원본 문서 등록 테스트")
 	void createOriginDocuments() {
 		// given
-		Integer docsId = 1;
 		String content = "<p>문서 내용</p>";
 
 		// when
@@ -191,7 +216,7 @@ class DocsServiceImplTest {
 		Mockito.when(userUtil.isAdmin(Mockito.any())).thenReturn(true);
 		User user = userUtil.getUser();
 
-		List<OriginDocumentDto> result = docsService.createOriginDocuments(docsId, content, user);
+		List<OriginDocumentDto> result = docsService.createOriginDocuments(doc2.getDocsId(), content, user1);
 
 		// then
 		assertThat(result).isNotNull();
@@ -201,11 +226,9 @@ class DocsServiceImplTest {
 	@Test
 	@DisplayName("특정 문서에 대한 번역 문서 목록 전체 조회 테스트")
 	void getAllTranslatedDocuments() {
-		// given
-		Integer docsId = 1;
 
 		// when
-		List<TranslatedDocumentDto> result = docsService.getAllTranslatedDocuments(docsId);
+		List<TranslatedDocumentDto> result = docsService.getAllTranslatedDocuments(doc1.getDocsId());
 
 		// then
 		assertThat(result).isNotNull();
@@ -215,11 +238,9 @@ class DocsServiceImplTest {
 	@Test
 	@DisplayName("특정 문서 내 베스트 번역본 조회 테스트")
 	void getBestTranslatedDocuments() {
-		// given
-		Integer docsId = 1;
 
 		// when
-		List<TranslatedDocumentDto> result = docsService.getBestTranslatedDocuments(docsId);
+		List<TranslatedDocumentDto> result = docsService.getBestTranslatedDocuments(doc1.getDocsId());
 
 		// then
 		assertThat(result).isNotNull();
@@ -231,12 +252,10 @@ class DocsServiceImplTest {
 	@DisplayName("번역 작성 테스트")
 	void createTranslatedDocument() {
 		// given
-		Integer docsId = 1;
-		Integer originId = 1;
 		String content = "새 번역 내용";
 
 		// when
-		TranslatedDocumentDto result = docsService.createTranslatedDocument(docsId, originId, user1, content);
+		TranslatedDocumentDto result = docsService.createTranslatedDocument(doc1.getDocsId(), originDoc2.getOriginId(), user2, content);
 
 		// then
 		assertThat(result).isNotNull();
@@ -247,13 +266,10 @@ class DocsServiceImplTest {
 	@WithMockCustomOAuth2User
 	@DisplayName("번역 수정 테스트")
 	void updateTranslatedDocument() {
-		// given
-		Integer docsId = 1;
-		Integer transId = 2;
 		String content = "수정된 번역 내용";
 
 		// when
-		TranslatedDocumentDto result = docsService.updateTranslatedDocument(docsId, transId, user2, content);
+		TranslatedDocumentDto result = docsService.updateTranslatedDocument(doc1.getDocsId(), Math.toIntExact(transDoc1.getTransId()), user2, content);
 
 		// then
 		assertThat(result).isNotNull();
@@ -264,12 +280,9 @@ class DocsServiceImplTest {
 	@WithMockCustomOAuth2User
 	@DisplayName("번역 삭제 테스트")
 	void deleteTranslatedDocument() {
-		// given
-		Integer docsId = 1;
-		Integer transId = 2;
 
 		// when
-		docsService.deleteTranslatedDocument(docsId, transId, user2);
+		docsService.deleteTranslatedDocument(doc1.getDocsId(), Math.toIntExact(transDoc1.getTransId()), user2);
 
 		// then
 		log.info("번역 삭제 성공");
@@ -279,12 +292,9 @@ class DocsServiceImplTest {
 	@WithMockCustomOAuth2User
 	@DisplayName("번역 좋아요 테스트")
 	void toggleVotes() {
-		// given
-		Integer docsId = 1;
-		Integer transId = 2;
 
 		// when
-		boolean result = docsService.toggleVotes(docsId, transId, user1);
+		boolean result = docsService.toggleVotes(doc1.getDocsId(), Math.toIntExact(transDoc1.getTransId()), user1);
 
 		// then
 		assertThat(result).isTrue();
