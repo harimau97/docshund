63ae7600ae9115e58a3994a693569ac0071823b0
이승연 <snowball@sookmyung.ac.kr>
2025-02-06 16:30:29 +0900

    Feat: 채팅 로그인한 User 확인 로직 수정

--- backend/docshund/src/main/java/com/ssafy/docshund/global/config/WebSocketConfig.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/global/config/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package com.ssafy.docshund.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.ssafy.docshund.global.util.stomp.StompHandler;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,6 +26,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-        registry.addEndpoint("/ws-connect").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-connect").setAllowedOriginPatterns("*");
+        // 개발에서는 다 열고 나중에 번역 쪽 문서만 가능하게 cors 닫기
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
