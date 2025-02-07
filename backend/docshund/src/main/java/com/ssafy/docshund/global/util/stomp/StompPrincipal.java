06be5904eab12c9a0ccc2a1a04a17a6360dfecda
이승연 <snowball@sookmyung.ac.kr>
2025-02-07 09:06:28 +0900

    Refactor: Stomp 로그인 세션 Accessor에 저장

---
+++ backend/docshund/src/main/java/com/ssafy/docshund/global/util/stomp/StompPrincipal.java
@@ -0,0 +1,17 @@
+package com.ssafy.docshund.global.util.stomp;
+
+import java.security.Principal;
+
+public class StompPrincipal implements Principal {
+
+    private final String name;
+
+    public StompPrincipal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
