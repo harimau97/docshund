9df088efd8d904e968cfd02bc6c462a489d63543
TinyFrogs <qns0147@gmail.com>
2025-02-09 01:26:13 +0900

    Feat: 닉네임 중복 체크 기능 구현

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/users/service/UserService.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/users/service/UserService.java
@@ -1,18 +1,18 @@
 package com.ssafy.docshund.domain.users.service;
 
-import com.ssafy.docshund.domain.users.dto.memo.MemoRequestDto;
-import com.ssafy.docshund.domain.users.dto.memo.MemoResponseDto;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.docshund.domain.users.dto.memo.MemoRequestDto;
+import com.ssafy.docshund.domain.users.dto.memo.MemoResponseDto;
 import com.ssafy.docshund.domain.users.dto.page.UserAndInfoDto;
 import com.ssafy.docshund.domain.users.dto.page.UserProfileDto;
 import com.ssafy.docshund.domain.users.dto.page.UserSearchCondition;
 import com.ssafy.docshund.domain.users.dto.profile.ProfileRequestDto;
 import com.ssafy.docshund.domain.users.entity.User;
-
-import java.util.List;
 
 public interface UserService {
 
@@ -22,18 +22,20 @@ public interface UserService {
 
 	public void modifyUserProfile(User user, ProfileRequestDto profileRequestDto, MultipartFile file);
 
+	public String duplicateNickname(String nickname);
+
 	// 메모 생성
-    void createMemo(Long userId, MemoRequestDto memoRequestDto);
+	void createMemo(Long userId, MemoRequestDto memoRequestDto);
 
 	// 메모 일괄 조회
 	List<MemoResponseDto> getMemos(Long userId);
 
 	// 메모 조회
 	MemoResponseDto getMemo(Long userId, Integer memoId);
-	
+
 	// 메모 수정
 	void modifyMemo(Long userId, Integer memoId, MemoRequestDto memoRequestDto);
-	
+
 	// 메모 삭제
 	void deleteMemo(Long userId, Integer memoId);
 }
