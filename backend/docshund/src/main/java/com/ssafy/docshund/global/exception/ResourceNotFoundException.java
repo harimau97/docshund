b92816e6f5af584b49436f0dc1f404547e09a6dd
TinyFrogs <qns0147@gmail.com>
2025-02-10 09:55:44 +0900

    Feat: 공통 예외처리 핸들러 구현

---
+++ backend/docshund/src/main/java/com/ssafy/docshund/global/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package com.ssafy.docshund.global.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class ResourceNotFoundException extends EntityNotFoundException {
+	private final ExceptionCode exceptionCode;
+
+	public ResourceNotFoundException(
+		ExceptionCode exceptionCode
+	) {
+		super(exceptionCode.getMessage());
+		this.exceptionCode = exceptionCode;
+	}
+
+	public ExceptionCode getExceptionCode() {
+		return exceptionCode;
+	}
+}
