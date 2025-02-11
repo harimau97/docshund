db452c87a0c1d4e8d8ec928d849db3621c2d7f17
이승연 <snowball@sookmyung.ac.kr>
2025-02-11 12:19:58 +0900

    Refactor: Validation 추가

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/chats/dto/ChatDto.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/chats/dto/ChatDto.java
@@ -1,5 +1,9 @@
 package com.ssafy.docshund.domain.chats.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatDto {
+    @NotNull(message = "DOCS ID CANNOT BE NULL")
+    @Positive(message = "DOCS ID MUST BE A POSITIVE NUMBER")
     private Integer docsId;
+
+    @NotBlank(message = "CHAT MESSAGE CANNOT BE BLANK")
+    @Size(max = 200, message = "CHAT MESSAGE IS TOO LONG")
     private String content;
 }
