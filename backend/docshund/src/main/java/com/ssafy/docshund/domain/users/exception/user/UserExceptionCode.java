4e413b2a374744322b466b1b51b588004ca04aaa
TinyFrogs <qns0147@gmail.com>
2025-02-14 10:56:15 +0900

    Fix: User 로그인 에러 추가 구현

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/users/exception/user/UserExceptionCode.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/users/exception/user/UserExceptionCode.java
@@ -1,7 +1,6 @@
 package com.ssafy.docshund.domain.users.exception.user;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.HttpStatus;
 
@@ -17,7 +16,9 @@ public enum UserExceptionCode implements ExceptionCode {
 	USER_NOT_FOUND(NOT_FOUND, "M-C-001", "회원을 찾을 수 없습니다."),
 	MEMBER_DUPLICATE_ERROR(BAD_REQUEST, "M-C-002", "중복된 닉네임입니다."),
 	NICKNAME_DUPLICATE_ERROR(BAD_REQUEST, "M-C-003", "중복된 이메일입니다."),
-	USER_INFO_NOT_FOUND(BAD_REQUEST, "M-C-004", "회원 정보를 찾을 수 없습니다.");
+	USER_INFO_NOT_FOUND(BAD_REQUEST, "M-C-004", "회원 정보를 찾을 수 없습니다."),
+	USER_BANNED(UNAUTHORIZED, "M-C-005", "정지된 유저입니다."),
+	USER_WITHDRAW(UNAUTHORIZED, "M-C-006", "탈퇴한 유저입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
