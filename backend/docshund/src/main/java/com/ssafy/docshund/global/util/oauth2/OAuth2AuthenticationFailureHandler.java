a1840e8da8d56bde4ec1e64b75c3317ad9ec522d
TinyFrogs <qns0147@gmail.com>
2025-02-14 20:29:55 +0900

    Fix: Update OAuth2AuthenticationException handling and add new properties file

--- backend/docshund/src/main/java/com/ssafy/docshund/global/util/oauth2/OAuth2AuthenticationFailureHandler.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/global/util/oauth2/OAuth2AuthenticationFailureHandler.java
@@ -5,6 +5,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -21,16 +22,19 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException exception) throws IOException, ServletException {
 
-		String message = exception.getMessage();
+		String errorCode = "UNKNOWN_ERROR";
+		if (exception instanceof OAuth2AuthenticationException) {
+			errorCode = ((OAuth2AuthenticationException)exception).getError().getErrorCode();
+		}
+
 		String redirectUrl;
-
 		log.info("exception {}", exception.toString());
-		log.info("message {}", message);
+		log.info("errorCode {}", errorCode);
 
-		if (message.startsWith("USER_BANNED")) {
+		if ("USER_BANNED".equals(errorCode)) {
 			redirectUrl = "https://i12a703.p.ssafy.io/error?status=403&message=" + URLEncoder.encode("계정이 정지되었습니다.",
 				StandardCharsets.UTF_8);
-		} else if (message.startsWith("USER_WITHDRAW")) {
+		} else if ("USER_WITHDRAW".equals(errorCode)) {
 			redirectUrl = "https://i12a703.p.ssafy.io/error?status=410&message=" + URLEncoder.encode("탈퇴한 계정입니다.",
 				StandardCharsets.UTF_8);
 		} else {
@@ -39,5 +43,6 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
 
 		getRedirectStrategy().sendRedirect(request, response, redirectUrl);
 	}
+
 }
 
