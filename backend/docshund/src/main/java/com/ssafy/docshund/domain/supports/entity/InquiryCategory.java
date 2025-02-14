5451b6ba66b9b6278eff813954a9dec8e1e51ac1
TinyFrogs <qns0147@gmail.com>
2025-02-14 13:04:00 +0900

    Fix: 문의 카테고리 이름 수정

--- backend/docshund/src/main/java/com/ssafy/docshund/domain/supports/entity/InquiryCategory.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/domain/supports/entity/InquiryCategory.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum InquiryCategory {
-	DOCUMENT_REQUEST("문서 등록 요청"), MEMBER("회원 관ㄹ녀"), REPORT("신고 관련");
+	DOCUMENT_REQUEST("문서 등록 요청"), MEMBER("회원 관련"), REPORT("신고 관련");
 
 	private final String description;
 
