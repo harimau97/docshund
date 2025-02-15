887ae43359d39a0120ded542fc27a87b1004db1a
조홍균 <honggyunjo1109@gmail.com>
2025-02-16 01:27:50 +0900

    Fix: 게시글 글자수 제한 수정

--- front/src/pages/community/writeArticle.jsx
+++ front/src/pages/community/writeArticle.jsx
@@ -94,8 +94,8 @@ const WriteArticle = () => {
       toast.info("모든 항목을 입력해주세요.");
       return;
     } else {
-      if (content.length > 10000) {
-        toast.info("글 내용은 10000자 이하로 작성해주세요.");
+      if (content.length > 15000) {
+        toast.info("글 내용은 15000자 이하로 작성해주세요.");
         return;
       }
 
