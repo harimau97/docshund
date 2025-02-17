6cafd7e162cda1881a5a8abf4be83a1e4462365b
TinyFrogs <qns0147@gmail.com>
2025-02-17 10:11:29 +0900

    Feat: 이메일 검증 서비스 구현

--- backend/docshund/src/main/java/com/ssafy/docshund/global/mail/MailSendService.java
+++ backend/docshund/src/main/java/com/ssafy/docshund/global/mail/MailSendService.java
@@ -2,15 +2,21 @@ package com.ssafy.docshund.global.mail;
 
 import static com.ssafy.docshund.global.mail.exception.MailExceptionCode.IMAGE_NOT_DOWNLOAD;
 import static com.ssafy.docshund.global.mail.exception.MailExceptionCode.MAIL_NOT_SEND;
+import static org.springframework.http.HttpStatus.OK;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.docshund.global.mail.exception.MailException;
 
 import jakarta.activation.DataSource;
@@ -27,8 +33,17 @@ public class MailSendService {
 
 	private final JavaMailSender mailSender;
 
+	@Value("${HUNTER_IO_SECRET_KEY}")
+	private String HUNTER_IO_KEY;
+
 	public void sendEmail(String sendEmail, String subject, String body, String imageUrl) {
 		try {
+			log.info("Validating email: " + sendEmail);
+
+			if (!isEmailValidWithHunter(sendEmail)) {
+				throw new MailException(MAIL_NOT_SEND);
+			}
+
 			log.info("Sending email to " + sendEmail);
 
 			MimeMessage message = mailSender.createMimeMessage();
@@ -57,5 +72,29 @@ public class MailSendService {
 		} catch (IOException e) {
 			throw new MailException(IMAGE_NOT_DOWNLOAD);
 		}
+	}
+
+	private boolean isEmailValidWithHunter(String email) {
+		String url = "https://api.hunter.io/v2/email-verifier?email=" + email + "&api_key=" + HUNTER_IO_KEY;
+
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+			if (response.getStatusCode() == OK) {
+				ObjectMapper objectMapper = new ObjectMapper();
+				JsonNode jsonNode = objectMapper.readTree(response.getBody());
+
+				// 응답에서 status 값이 "valid"인지 확인
+				String status = jsonNode.path("data").path("status").asText();
+				log.info("status {}", status);
+				return "valid".equals(status);
+			}
+		} catch (Exception e) {
+			log.error("Failed to validate email: " + email, e);
+			throw new MailException(MAIL_NOT_SEND);
+		}
+
+		return false; // 실패 시 기본적으로 유효하지 않은 것으로 간주
 	}
 }
