2907cdf801a8f279bb39b64a55bf85069ce2ee4e
이시호 <harimauho@gmail.com>
2025-02-20 17:06:29 +0900

    !Hotfix: 문서 등록후 리로드 하도록 수정

--- front/src/pages/admin/components/registDocs.jsx
+++ front/src/pages/admin/components/registDocs.jsx
@@ -50,14 +50,9 @@ const RegistDocs = ({ open, onClose }) => {
       toast.success("문서 등록 성공", {
         toastId: "registDocs",
       });
-      formData.documentCategory = "";
-      formData.documentName = "";
-      formData.documentVersion = "";
-      formData.documentLink = "";
-      formData.documentLogo = "";
-      formData.license = "";
-      formData.position = "";
-      await fetchDocsList();
+
+      // await fetchDocsList();
+      window.location.reload();
       onClose();
     } else {
       toast.error("문서 등록 실패", {
